6ec208b0e0788843b08d896082117f6572a38a77
Milton <ajaxmilton@hotmail.com>
2024-08-15 23:48:02 +0200

    Fix code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,42 +4,35 @@
 
 import sys
 
+def print_stats(total_size, status_counts):
+    print(f"File size: {total_size}")
+    for status in sorted(status_counts.keys()):
+        if status_counts[status] > 0:
+            print(f"{status}: {status_counts[status]}")
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+total_size = 0
+status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
+        parts = line.split()
+        if len(parts) < 7:
+            continue
+        ip, dash, date, method, url, protocol, status, size = parts[:8]
         try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
+            size = int(size)
+            status = int(status)
+            total_size += size
+            if status in status_counts:
+                status_counts[status] += 1
+        except ValueError:
+            continue
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats(total_size, status_counts)
 except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    print_stats(total_size, status_counts)
+    sys.exit(0)
+
+print_stats(total_size, status_counts)
