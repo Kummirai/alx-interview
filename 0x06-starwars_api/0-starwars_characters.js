d395598f8dfb2ecb456e081aac2515a1a2bbeca8
Kummirai <ajaxmilton@hotmail.com>
2024-09-05 21:34:45 +0200

    Fix code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,53 @@
-#!/usr/bin/node
+// #!/usr/bin/node
+
+// const request = require('request');
+
+// const movieId = process.argv[2];
+
+// const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+// request(url, async (err, res, body) => {
+//   err && console.log(err);
+
+//   const charactersArray = (JSON.parse(res.body).characters);
+//   for (const character of charactersArray) {
+//     await new Promise((resolve, reject) => {
+//       request(character, (err, res, body) => {
+//         err && console.log(err);
+
+//         console.log(JSON.parse(body).name);
+//         resolve();
+//       });
+//     });
+//   }
+// });
+
 
 const request = require('request');
 
-const movieId = process.argv[2];
+function getCharacters(movieId) {
+    const url = `https://swapi.dev/api/films/${movieId}/`;
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+    request(url, { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(`Error: Unable to fetch data for movie ID ${movieId}`);
+        }
 
-request(url, async (err, res, body) => {
-  err && console.log(err);
-
-  const charactersArray = (JSON.parse(res.body).characters);
-  for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        err && console.log(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
+        const characters = body.characters;
+        characters.forEach(characterUrl => {
+            request(characterUrl, { json: true }, (err, res, characterBody) => {
+                if (err) {
+                    return console.log(`Error: Unable to fetch character data from ${characterUrl}`);
+                }
+                console.log(characterBody.name);
+            });
+        });
     });
-  }
-});
+}
+
+const movieId = process.argv[2];
+if (!movieId) {
+    console.log("Usage: node script.js <movie_id>");
+} else {
+    getCharacters(movieId);
+}
