428572827ac63f693463a6a1476362cd51e28911
Kummirai <ajaxmilton@hotmail.com>
2024-09-19 23:06:37 +0200

    Add makeChange function to calculate required change total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,6 +4,9 @@
 """
 
 def makeChange(coins, total):
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
+    """
     if total <= 0:
         return 0
 
